2e4eae4ea439ffadb7da6fe35f3b9a8e0cce65cc
khemkhadka <kkhadka@uncc.edu>
2023-04-12 22:35:08 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -150,7 +150,7 @@ resource "azurerm_cognitive_account" "cs" {
 // Azure Synapse -- Khem 
 
 resource "azurerm_storage_account" "gen2storage" {
-  name                     = "${var.student_name}-${var.environment}-${random_integer.deployment_id_suffix.result}_st2"
+  name                     = "${var.student_name}${var.environment}${random_integer.deployment_id_suffix.result}st2"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
